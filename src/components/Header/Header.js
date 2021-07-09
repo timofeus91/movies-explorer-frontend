7a3074eb2bd135df235ceec01f6b867935236d4a
Timofey Berezhnov <timofeus91@gmail.com>
2021-07-09 15:12:31 +0400

    css in progress n7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,17 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header({ loggedIn, filmRoute }) {
+
+const [ isSidebarOpen, setIsSidebarOpen ] = React.useState(false);
+
+function handleOpenSidebar() {
+  setIsSidebarOpen(true);
+}
+
+function handleCloseSidebar() {
+  setIsSidebarOpen(false);
+}
+
     return (
       <header className="header">
         {loggedIn ? (
@@ -14,7 +25,17 @@ function Header({ loggedIn, filmRoute }) {
             <Link to='/saved-movies' className={`header__link header__link_saved-movies ${filmRoute ? 'header__link_saved-movies-noactive' : ''}`}>Сохранённые фильмы</Link>
           </div>
           <Link to='/profile' className="header__link header__link_profile">Аккаунт</Link>
-          <button className="header__sidebar-button" type="button"></button>
+          <button className="header__sidebar-open" type="button" onClick={handleOpenSidebar}></button>
+
+          <div className="header__sidebar">
+            <button className="header__sudebar-close" type="button" onClick={handleCloseSidebar}></button>
+            <div className="header__sidebar-container">
+              <Link to='/' className="header__link header__link-main">Главная</Link>
+              <Link to='/movies' className={`header__link header__link_movies ${filmRoute ? '' : 'header__link_movies-noactive'} `}>Фильмы</Link>
+              <Link to='/saved-movies' className={`header__link header__link_saved-movies ${filmRoute ? 'header__link_saved-movies-noactive' : ''}`}>Сохранённые фильмы</Link>
+              <Link to='/profile' className="header__link header__link_profile">Аккаунт</Link>
+            </div>
+          </div>
           </>
         ) : (
           <>
