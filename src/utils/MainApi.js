e1361d352851d5c7c7cd9c0486cc31227882d273
Timofey Berezhnov <timofeus91@gmail.com>
2021-07-20 17:39:34 +0400

    first version api is ready

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -27,7 +27,7 @@ class MainApi {
                 name,
                 email,
                 password,
-            })
+            }),
         })
         .then(this._checkResponse);
     }
@@ -40,7 +40,7 @@ class MainApi {
             body: JSON.stringify({
                 email,
                 password,
-            })
+            }),
         })
         .then(this._checkResponse);
     }
@@ -53,7 +53,7 @@ class MainApi {
             body: JSON.stringify({
                 email,
                 name,
-            })
+            }),
         })
         .then(this._checkResponse);
     }
@@ -67,94 +67,58 @@ class MainApi {
         .then(this._checkResponse);
     }
 
+    //метод по проверке токена
+    checkToken(jwt)  {
+        return fetch(`${this._url}users/me`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${jwt}`,
+          },
+        })
+        .then(this._checkResponse);
+      }
 
 
-    //метод по добавлению новой карточки
+      //метод по получению списка фильмов
+      getMovies() {
+          return fetch(`${this._url}movies`, {
+              method: 'GET',
+              headers: this._headers,
+          })
+          .then(this._checkResponse);
+      }
 
-    addNewCard(data) {
-        return fetch(`${this._url}cards`, {
+      //метод по добавлению фильма
+      createMovie(data) {
+          return fetch(`${this._url}movies`, {
               method: 'POST',
               headers: this._headers,
               body: JSON.stringify({
-                  name: data.name,
-                  link: data.link,
-              })
-        }).then(this._checkResponse);
-    }
+                  country: data.country,
+                  director: data.director,
+                  duration: data.duration,
+                  year: data.year,
+                  description: data.description,
+                  image: data.image,
+                  trailer: data.trailer,
+                  thumbnail: data.thumbnail,
+                  movieId: data.movieId,
+                  nameRU: data.nameRU,
+                  nameEN: data.nameEN,
+              }),
+          })
+          .then(this._checkResponse);
+      }
 
-    //метод по получению информации о пользователе
-
-    getUserInfo() {
-        return fetch(`${this._url}users/me`, {
-            method: 'GET',
-            headers: this._headers,
-        }).then(this._checkResponse);
-    }
-
-    //метод по изменению информации о пользователе
-
-    editUserInfo(data) {
-        return fetch(`${this._url}users/me`, {
-            method: 'PATCH',
-            headers: this._headers,
-            body: JSON.stringify({
-                name: data.name,
-                about: data.about,
-              })
-        }).then(this._checkResponse);
-    }
-
-    //метод по смене аватарки
-
-    editUserAvatar(data) {
-        return fetch(`${this._url}users/me/avatar`, {
-            method: 'PATCH',
-            headers: this._headers,
-            body: JSON.stringify({
-                avatar: data.avatar
-              })
-        }).then(this._checkResponse);
-    }
-
-
-    //приватный метод по установке лайка
-
-    _putLike(cardId) {
-        return fetch(`${this._url}cards/${cardId}/likes/`, {
-            method: 'PUT',
-            headers: this._headers,
-      }).then(this._checkResponse);
-    }
-
-    //приватный метод по снятию лайка
-
-    _removeLike(cardId) {
-        return fetch(`${this._url}cards/${cardId}/likes/`, {
-            method: 'DELETE',
-            headers: this._headers,
-      }).then(this._checkResponse);
-    }
-
-
-    //Единый метод по установке и снятию лайка
-
-    changeLikeCardStatus(cardId, isLiked) {
-        if (isLiked) {
-           return this._putLike(cardId);
-        }
-        else {
-            return this._removeLike(cardId);
-        }
-    }
-
-    //метод по удалению карточки
-
-    deleteCard(cardId) {
-        return fetch(`${this._url}cards/${cardId}/`, {
-            method: 'DELETE',
-            headers: this._headers,
-      }).then(this._checkResponse);
-    }
+      //метод по удалению фильма
+      deleteMovie(movieId) {
+          return fetch(`${this._url}movies/${movieId}`, {
+              method: 'DELETE',
+              headers: this._headers,
+          })
+          .then(this._checkResponse);
+      }
 
 
 }
