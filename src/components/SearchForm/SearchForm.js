4e7be734a3923465f4efd702e66c08d73474ae2b
Timofey Berezhnov <timofeus91@gmail.com>
2021-07-28 18:51:36 +0400

    validation searchform is ready

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,29 @@
 import React from "react";
 import icon from "../../images/searchIcon.svg";
 import './SearchForm.css';
+import { Validation } from '../../utils/Validation';
 
-function SearchForm() {
+function SearchForm({ onQuery }) {
+
+  //вытаскиваем все из Validation
+  const { values, errors, isValid, handleChange, setValues } = Validation();
+
+   //функция по сбрасыванию действия по умолчанию и для передачи значений инпутов в следующий компонент
+   function handleSubmit(e) {
+    // Запрещаем браузеру переходить по адресу формы
+    e.preventDefault();
+    // Передаём значения управляемых компонентов во внешний обработчик
+    onQuery(values);
+  }
+
     return (
       <section className="search">
         <div className="search__flex-container">
-        <form className="search__form">
+        <form className="search__form" onSubmit={handleSubmit} noValidate={true}>
           <div className="search__input-container">
-            <img className="searc__logo" alt='Лупа' src={icon} />
-            <input className="search__input" placeholder='Фильм' required />
+            <img className="search__logo" alt='Лупа' src={icon} />
+            <input name='query' id='query' autoComplete="off" value={values.query || ''} onChange={handleChange} className="search__input" placeholder='Фильм' required={true} minLength="1" maxLength='132' type='text' />
+            <span className={`search__error-label ${errors.query ? 'search__error-label_active' : ''}`}>{errors.query}</span>
           </div>
           <div className="search__submit-container">
             <button className="search__submit" type="submit"></button>
