5a4d345cdfe936de37bcc6a605cd72864a8a977f
Timofey Berezhnov <timofeus91@gmail.com>
2021-09-05 16:33:41 +0400

    showMore is ready

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,21 +3,78 @@ import MoviesCard from "../MoviesCard/MoviesCard";
 import './MoviesCardList.css';
 import { useLocation } from 'react-router-dom';
 
+
+
 function MoviesCardList({moviesCards, handleLikeMovie, handleDelete }) {
    //хук по проверке роута
    const routeWay = useLocation().pathname;
 
-   const moviesCardsRender = moviesCards.slice(0, 5)
 
-   const elseShow = true;
+
+
+   const [movieCount, setMovieCount] = React.useState(defaultResize);
+
+   const moviesCardsRender = moviesCards.slice(0, movieCount)
+
+
+   //функция которая ставится по умолчанию в movieCount для отображения нужного количества карточек по умолчанию в зависимости от размера окна в момент
+   function defaultResize() {
+    const windowInnerWidth = window.innerWidth;
+    
+    switch (true) {
+      case (windowInnerWidth < 600):
+        return 5
+      case (windowInnerWidth < 900):
+        return 8
+      case (windowInnerWidth < 1280):
+        return 12
+      default: return 12
+
+    }
+   }
+
+   function elseShow() {
+    if(moviesCardsRender.length < moviesCards.length ) {
+      return true
+    }
+    else {
+      return false
+    }
+   }
+   //функция которая будет менять количество отображаемых карточек по умолчанию в случае изменения размера окна 
+   function onResize() {
+    const windowInnerWidth = window.innerWidth;
+    switch (true) {
+      case (windowInnerWidth < 600):
+        return setMovieCount(5)
+      case (windowInnerWidth < 900):
+        return setMovieCount(8)
+      case (windowInnerWidth < 1280):
+        return setMovieCount(12)
+      default: setMovieCount(12)
+    }
+   }
+
+
 
    function elseClick() {
-     console.log('пока просто клик');
+    const windowInnerWidth = window.innerWidth;
+
+    if(windowInnerWidth < 769) {
+      setMovieCount(movieCount +2)
+    }
+    else {
+      setMovieCount(movieCount +3)
+    }
    }
 
    //проверяем делать ли кнопку "еще" неактивной сразу 
    const showMore = routeWay === '/saved-movies' ? 'movies__button_disabled' : (elseShow ? 'movies__button' : 'movies__button_disabled');
   
+   //вешаем слушатель на окно через эффект. 
+   React.useEffect(() => {
+    window.addEventListener('resize', onResize)
+   }, [])
 
 
     return (
